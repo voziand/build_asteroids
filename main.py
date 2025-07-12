756ef4bf2327b1e812ee92ee471dea0cc40c664e
voziand <denis.vozian@gmail.com>
2025-07-11 19:47:11 -0700

    it creates a window!

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from constants import *
+
+window = pygame.Surface((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        window.fill('black')
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
